357e0d966eb6ba59b0910e530f2b4fbeddeac356
John Paul Gabule <lucas.gabule@gmail.com>
2025-11-24 12:14:42 +0800

    feat: add vanilla js for state management system

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -17,10 +17,73 @@ export type ExtractState<S> = S extends { getState: () => infer T } ? T : never;
 
 type Get<T, K, F> = K extends keyof T ? T[K] : F;
 
-// export type Mutate<S, Ms> = number extends Ms["length" & keyof Ms]
-//   ? S
-//   : Ms extends []
-//   ? S
-//   : Ms extends [[infer Mi, infer Ma], ...infer Mrs]
-//   ? Mutate<StoreMutators<S, Ma>[Mi & StoreMutatorIdentifier], Mrs>
-//   : never;
+export interface StoreMutators<S, A> {}
+export type StoreMutatorIdentifier = keyof StoreMutators<unknown, unknown>;
+
+export type Mutate<S, Ms> = number extends Ms["length" & keyof Ms]
+  ? S
+  : Ms extends []
+  ? S
+  : Ms extends [[infer Mi, infer Ma], ...infer Mrs]
+  ? Mutate<StoreMutators<S, Ma>[Mi & StoreMutatorIdentifier], Mrs>
+  : never;
+
+export type StateCreator<
+  T,
+  Mis extends [StoreMutatorIdentifier, unknown][] = [],
+  Mos extends [StoreMutatorIdentifier, unknown][] = [],
+  U = T
+> = ((
+  setState: Get<Mutate<StoreApi<T>, Mis>, "setState", never>,
+  getState: Get<Mutate<StoreApi<T>, Mis>, "getState", never>,
+  store: Mutate<StoreApi<T>, Mis>
+) => U) & { $$storeMutators?: Mos };
+
+type CreateStore = {
+  <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
+    initializer: StateCreator<T, [], Mos>
+  ): Mutate<StoreApi<T>, Mos>;
+};
+
+type CreateStoreImpl = <
+  T,
+  Mos extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  initializer: StateCreator<T, [], Mos>
+) => Mutate<StoreApi<T>, Mos>;
+
+const createStoreImpl: CreateStoreImpl = (createState) => {
+  type TState = ReturnType<typeof createState>;
+  type Listener = (state: TState, prevState: TState) => void;
+  let state: TState;
+
+  const listeners: Set<Listener> = new Set();
+
+  const setState: StoreApi<TState>["setState"] = (partial, replace) => {
+    const nextState =
+      typeof partial === "function"
+        ? (partial as (state: TState) => TState)(state)
+        : partial;
+
+    if (!Object.is(nextState, state)) {
+      const previousState = state;
+      state =
+        replace ?? (typeof nextState !== "object" || nextState === null)
+          ? (nextState as TState)
+          : Object.assign({}, state, nextState);
+      listeners.forEach((listener) => listener(state, previousState));
+    }
+  };
+  const getState: StoreApi<TState>["getState"] = () => state;
+  const getInitialState: StoreApi<TState>["getInitialState"] = () =>
+    initialState;
+  const subscribe: StoreApi<TState>["subscribe"] = (listener) => {
+    listeners.add(listener);
+    // unsubscribe
+    return () => listeners.delete(listener);
+  };
+  const api = { setState, getState, getInitialState, subscribe };
+  const initialState = (state = createState(setState, getState, api));
+
+  return api as any;
+};
