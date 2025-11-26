1f5832332efaf0def1bd17e563d74d2cc417dc06
John Paul Gabule <lucas.gabule@gmail.com>
2025-11-26 17:53:03 +0800

    feat: add devtools

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,3 +3,10 @@ declare interface ImportMeta {
     MODE: string;
   };
 }
+interface Window {
+  __REDUX_DEVTOOLS_EXTENSION__?: {
+    connect(options?: any): any;
+    disconnect(): void;
+  };
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
