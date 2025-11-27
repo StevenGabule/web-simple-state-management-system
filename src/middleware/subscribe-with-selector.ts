ef4068a63d4b8ba02548c4324e4690fa602855f2
John Paul Gabule <lucas.gabule@gmail.com>
2025-11-28 04:26:48 +0800

    add middleware subscribe with selector

---
+++ src/middleware/subscribe-with-selector.ts
@@ -0,0 +1,78 @@
+import { StateCreator, StoreMutatorIdentifier } from "../vanilla";
+
+type SubscribeWithSelector = <
+  T,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  initializer: StateCreator<
+    T,
+    [...Mps, ["zustand/subscribeWithSelector", never]],
+    Mcs
+  >
+) => StateCreator<T, Mps, [["zustand/subscribeWithSelector", never], ...Mcs]>;
+
+type Write<T, U> = Omit<T, keyof U> & U;
+
+type WithSelectorSubscribe<S> = S extends { getState: () => infer T }
+  ? Write<S, StoreSubscribeWithSelector<T>>
+  : never;
+
+type StoreSubscribeWithSelector<T> = {
+  subscribe: {
+    (
+      listener: (selectedState: T, previousSelectedState: T) => void
+    ): () => void;
+    <U>(
+      selector: (state: T) => U,
+      listener: (selectedState: U, previousSelectedState: U) => void,
+      options?: {
+        equalityFn?: (a: U, b: U) => boolean;
+        fireImmediately?: boolean;
+      }
+    ): () => void;
+  };
+};
+
+declare module "../vanilla" {
+  interface StoreMutators<S, A> {
+    ["zustand/subscribeWithSelector"]: WithSelectorSubscribe<S>;
+  }
+}
+
+type SubscribeWithSelectorImpl = <T extends object>(
+  storeInitializer: StateCreator<T, [], []>
+) => StateCreator<T, [], []>;
+
+const subscribeWithSelectorImpl: SubscribeWithSelectorImpl =
+  (fn) => (set, get, api) => {
+    type S = ReturnType<typeof fn>;
+    type Listener = (state: S, previousState: S) => void;
+    const origSubscribe = api.subscribe as (listener: Listener) => () => void;
+    api.subscribe = ((selector: any, optListener: any, options: any) => {
+      let listener: Listener = selector;
+      if (optListener) {
+        const equalityFn = options.equalityFn || Object.is;
+        let currentSlice = selector(api.getState());
+        listener = (state) => {
+          const nextSlice = selector(state);
+          if (!equalityFn(currentSlice, nextSlice)) {
+            const previousSlice = currentSlice;
+            optListener((currentSlice = nextSlice), previousSlice);
+          }
+        };
+
+        if (options?.fireImmediately) {
+          optListener(currentSlice, currentSlice);
+        }
+      }
+
+      return origSubscribe(listener);
+    }) as any;
+
+    const initialState = fn(set, get, api);
+    return initialState;
+  };
+
+export const subscribeWithSelector =
+  subscribeWithSelectorImpl as unknown as SubscribeWithSelector;
